62f4c15edf1da3a385e9774f26b1450c79fdaf8d
Patrice de Saint Steban <patrice.desaintsteban@zenika.com>
2018-02-26 21:13:27 +0100

    Input output

--- src/main/java/hashcode/Slice.java
+++ src/main/java/hashcode/Slice.java
@@ -7,4 +7,15 @@ public class Slice {
     int r2;
     int tomatoes;
     int mushrooms;
+
+    public Slice() {
+
+    }
+
+    public Slice(int r1, int c1, int r2, int c2) {
+        this.r1 = r1;
+        this.c1 = c1;
+        this.r2 = r2;
+        this.c2 = c2;
+    }
 }
