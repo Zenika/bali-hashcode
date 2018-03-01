11033fa4409443ebfc04994820bf011439689f47
Florian Negre <florian.negre@zenika.com>
2018-03-01 20:29:10 +0100

    WIP

--- src/main/java/hashcode/RideFinder.java
+++ src/main/java/hashcode/RideFinder.java
@@ -7,10 +7,10 @@ public class RideFinder {
     public static Optional<Ride> findClosestRide(City city, int step, Vehicle vehicle) {
         Ride best = null;
         for (Ride ride : city.rides) {
-            if (!ride.available || step > ride.latestFinish || step + Simulation.nbStepNecessary(ride, vehicle) > city.steps) {
+            if (!ride.available || step > ride.latestFinish || step + Simulation.nbStepNecessary(ride, vehicle) > ride.latestFinish) {
                 continue;
             }
-            Simulation.log("ok find closest");
+            //Simulation.log("ok find closest");
 
             if(step + Simulation.getDistanceFromStart(vehicle, ride) < ride.earlestStart) {
                 continue;
