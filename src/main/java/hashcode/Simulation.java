4cc927a63de54cc4bfa0e3d8177723c8cd431b63
Vincent Poilvert <v.poilvert@gmail.com>
2018-03-01 19:02:24 +0100

    Add simulation

---
+++ src/main/java/hashcode/Simulation.java
@@ -0,0 +1,19 @@
+package hashcode;
+
+public class Simulation {
+
+    private City city;
+
+    public Simulation(City city) {
+        this.city = city;
+    }
+
+    public void resolve() {
+
+    }
+
+
+    public boolean isEnded() {
+        return true;
+    }
+}
