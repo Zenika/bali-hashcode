ed7acd88bdac84b44b107ded852b43712efbd42e
Vincent Poilvert <v.poilvert@gmail.com>
2018-03-01 20:30:30 +0100

    WIP

--- src/main/java/hashcode/RideFinder.java
+++ src/main/java/hashcode/RideFinder.java
@@ -1,23 +1,31 @@
 package hashcode;
 
+import hashcode.training.Solution;
+
 import java.util.Optional;
 
 public class RideFinder {
 
     public static Optional<Ride> findClosestRide(City city, int step, Vehicle vehicle) {
         Ride best = null;
+
         for (Ride ride : city.rides) {
             if (!ride.available || step > ride.latestFinish || step + Simulation.nbStepNecessary(ride, vehicle) < city.steps) {
                 continue;
             }
 
-            if(step + Simulation.getDistanceFromStart(vehicle, ride) < ride.earlestStart) {
+            int distanceFromStart = Simulation.getDistanceFromStart(vehicle, ride);
+            int startStep = step + distanceFromStart;
+            if(startStep < ride.earlestStart) {
                 continue;
             }
 
 
-            best = ride;
-            break;
+            if(best == null) {
+                best = ride;
+            } else if(distanceFromStart < Simulation.getDistanceFromStart(vehicle, best)) {
+                best = ride;
+            }
         }
 
         return Optional.ofNullable(best);
