b1d661e4eb76a2912d158879c12c98f2910f165c
Florian Negre <florian.negre@zenika.com>
2018-03-01 19:11:22 +0100

    WIP

--- src/main/java/hashcode/Vehicle.java
+++ src/main/java/hashcode/Vehicle.java
@@ -6,7 +6,8 @@ public class Vehicle {
     List<Ride> rides;
     int currentRow;
     int currentColumn;
+
     boolean couldBeOnTime(int currentStep, Ride ride) {
-        return true;
+        return (Simulation.getDistanceFromEnd(this, ride) > ride.latestFinish - Math.max(currentStep, ride.earlestStart));
     }
 }
