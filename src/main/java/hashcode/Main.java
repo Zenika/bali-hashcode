bac1d04fc5bde7dbedb1776e1370a8f50178cc28
Patrice de Saint Steban <patrice.desaintsteban@zenika.com>
2018-03-01 18:59:23 +0100

    reader

--- src/main/java/hashcode/Main.java
+++ src/main/java/hashcode/Main.java
@@ -5,6 +5,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] argv) {
         Scanner scanner = new Scanner(System.in);
+        City city = new City();
+        city.rows = scanner.nextInt();
+        city.columns = scanner.nextInt();
+        city.nbVehicules = scanner.nextInt();
+        city.nbRides = scanner.nextInt();
+        city.rideBonus = scanner.nextInt();
+        city.steps = scanner.nextInt();
+        scanner.nextLine();
+        for(int i = 0; i < city.nbRides; i++) {
+            Ride ride = new Ride();
+            ride.rowStart = scanner.nextInt();
+            ride.columnStart = scanner.nextInt();
+            ride.rowEnd = scanner.nextInt();
+            ride.columnEnd = scanner.nextInt();
+            ride.earlestStart = scanner.nextInt();
+            ride.latestStart = scanner.nextInt();
+        }
 
         // TODO Resolve
     }
